ac3342123d92fa8c4153e259de0a9e435121ce22
Felipe Villarroel <fe.villarroelb@duocuc.cl>
2024-05-07 16:42:04 +0000

    Update calculadora_descuento.py

--- ejercicios 06-05-2024/calculadora_descuento.py
+++ ejercicios 06-05-2024/calculadora_descuento.py
@@ -1,20 +1,22 @@
 while True:
     try:
         precio = float(input("Ingrese el valor del producto"))
+        break
     except ValueError:
         print("no es float, favor ingrese un numero!")
         continue
     else:
-        break
+        return 1
 
 while True:
     try:
         descuento = float(input("Ingrese el porcentaj del descuento (numero decimal entre 0 y 1)"))
+        break
     except ValueError:
         print("No es float, ingrese un numero")
         continue
     else:
-        break
+        return 1
 
 valor_final = precio - (precio*descuento)
 print(f"El valor final es :" f"${int(valor_final):,}".replace(",","."))
