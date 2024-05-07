a4545c5a892567a081f0b26782a75ccd7d59a85e
Felipe Villarroel <fe.villarroelb@duocuc.cl>
2024-05-07 00:57:21 +0000

    Update contar_vocales.py

--- ejercicios 06-05-2024/contar_vocales.py
+++ ejercicios 06-05-2024/contar_vocales.py
@@ -1,16 +1,20 @@
 palabras = ["Hola","holis","arbol",
             "murcielago","panda","perro",
-            "olores","HOLIWIS","asdsadsadsa",
+            "olores","negro","asdsadsadsa",
             "murciegalookgoskgofskgofskodkfokfoakf",
-            "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"]
+            "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa",222]
 cantidad_vocales = 0
 cantidad_consonantes = 0
 
 for palabra in palabras:
-    for i in palabra:
-        if i in "aeiou":
-            cantidad_vocales +=1
-        else:
-            cantidad_consonantes +=1
+    print(f"Revisando {palabra} la cual es {type(palabra)}")
+    if isinstance(palabra,int) == False :
+        for i in palabra:
+            if i in "aeiou":
+                cantidad_vocales +=1
+            else:
+                cantidad_consonantes +=1
+    else:
+        continue
 
 print(f"Cantidad de vocales : {cantidad_vocales} \n Cantidad de consonantes : {cantidad_consonantes}")
