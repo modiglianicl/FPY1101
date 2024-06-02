5a80b1b3dfa5b7c1f706ca44de52421a10a1f44c
modiglianicl <vilarroel.fel@gmail.com>
2024-06-02 10:14:45 -0400

    update ejercicio_3.py

--- Unidad 3/3.1 Colecciones en Python/3.1.3/ejercicio_3.py
+++ Unidad 3/3.1 Colecciones en Python/3.1.3/ejercicio_3.py
@@ -16,4 +16,31 @@ for i in range(len(bus)):
                 print(f" ",end="")
             print(f"{bus[i][j]}",end="  ")
 
-print(bus)
+print(f"--- Sin comprension de listas ---")
+# Sin comprension de listas
+
+bus_fome = []
+
+## Creamos las 10 filas
+for i in range(10): 
+    bus_fome.append([])
+
+## Creamos las 4 columnas
+for i in range(len(bus_fome)):
+    for j in range(4):
+        bus_fome[i].append((j+1)+(4*i)) # Parte en 1, y termina en 40
+
+for i in range(len(bus_fome)):
+    for j in range(len(bus[i])):
+        if j == 1:
+            if bus_fome[i][j] < 10:
+                print(f" ",end="")
+            print(f"{bus_fome[i][j]}",end="     | |     ")
+        elif j == 3:
+            if bus_fome[i][j] < 10:
+                print(f" ",end="")
+            print(f"{bus_fome[i][j]}")
+        else:
+            if bus_fome[i][j] < 10:
+                print(f" ",end="")
+            print(f"{bus_fome[i][j]}",end="  ")
