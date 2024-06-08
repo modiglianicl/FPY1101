ef9a32cd0f6a38bdc960d21a91ef4efa0efc1818
modiglianicl <vilarroel.fel@gmail.com>
2024-06-07 21:46:37 -0400

    update ejercicio_2.py

--- Unidad 3/3.2 Trabajando con diccionarios , set y tuplas/ejercicio_2.py
+++ Unidad 3/3.2 Trabajando con diccionarios , set y tuplas/ejercicio_2.py
@@ -1,22 +1,19 @@
 def esPrimo(listaNumeros):
-    if len(listaNumeros) >= 2:
+    if len(listaNumeros) >= 1:
         conjuntoPrimos = set()
         for i in listaNumeros:
             divisoresCero = 0
             if i == 0:
                 continue
             else:
-                for j in listaNumeros:
+                for j in range(1,i+1):
                     if j == 0:
                         continue
                     elif i % j == 0:
                         divisoresCero +=1
-                    else:
-                        continue
             if divisoresCero == 2:
                 conjuntoPrimos.add(i)
-            else:
-                continue
+                
         if len(conjuntoPrimos) == 0:
             print(f"No hay primos en ese rango!")
             print(conjuntoPrimos)
@@ -25,4 +22,4 @@ def esPrimo(listaNumeros):
     else:
         print(f"La lista debe tener al menos 2 numeros")
 
-esPrimo(range(50,100))
+esPrimo(range(1,26))
