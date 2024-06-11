749f8919e55ea0c2bf1ada22b768eeec84b6a031
modiglianicl <vilarroel.fel@gmail.com>
2024-06-11 17:01:00 -0400

    update creacion_csv_profe.py

--- Unidad 3/3.3 Uso de archivos en Python/creacion_csv_profe.py
+++ Unidad 3/3.3 Uso de archivos en Python/creacion_csv_profe.py
@@ -3,7 +3,7 @@ import json
 
 with open('ejercicio.csv', "w", newline='') as x:
     escritura = csv.writer(x)
-    
+    escritura.writerow(['Nombre','edad','Ciudad'])
     escritura.writerows([
 ['Esteban', 25, 'Santiago'],
 ['María', 30, 'Valparaíso'],
