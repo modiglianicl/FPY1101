b87b3331daaca0534f65967f3ab827a279a65e70
modiglianicl <vilarroel.fel@gmail.com>
2024-06-15 11:57:09 -0400

    update ejercicio_2.py

--- Unidad 3/3.3 Uso de archivos en Python/clase 14-06 y 15-06/ejercicio_2.py
+++ Unidad 3/3.3 Uso de archivos en Python/clase 14-06 y 15-06/ejercicio_2.py
@@ -1,21 +1,25 @@
+# Libreria necesaria
 import csv
 
+# Inicializa lista de datos
 lista_biologia = []
 
 
-
+# Lee el archivo y apenda solo en donde cada elemento 3 de cada fila sea "Biología"
 with open('estudiantes.csv', 'r', newline='', encoding='utf-8') as archivo:
     datos = csv.reader(archivo)
     for i in datos:
         if i[2] == "Biología":
             lista_biologia.append(i)
 
-suma = 0
+# Variable donde guarda el promedio maximo
 promedio_alto = 0
 
+# Iteracion sobre cada alumno de biologia
 for dato in lista_biologia:
-    promedio = (float(dato[3]) + float(dato[4]) + float(dato[5]))/3
-    if promedio > promedio_alto:
+    promedio = (float(dato[3]) + float(dato[4]) + float(dato[5]))/3 # Saca el promedio casteando float en cada nota, las cuales estan en indicies 3,4,5
+    if promedio > promedio_alto: # Algoritmo de maximo
         promedio_alto = promedio
 
-print(promedio_alto)
+# Imprime el maximo
+print(round(promedio_alto,2))
