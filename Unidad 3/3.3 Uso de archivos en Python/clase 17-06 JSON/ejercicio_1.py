ebf4b53e0cd1640da16b04ccd3a061434529df89
Vina del Mar <vina@duoc.acad>
2024-06-17 20:53:03 -0400

    add ejercicio tipo prueba

--- Unidad 3/3.3 Uso de archivos en Python/clase 17-06 JSON/ejercicio_1.py
+++ Unidad 3/3.3 Uso de archivos en Python/clase 17-06 JSON/ejercicio_1.py
@@ -1,20 +1,23 @@
 import json
-
+# Inicializo lista para agregar cada diccionario
 json_dump = []
 
+# Abro el archivo
 with open('archivo.json','r',encoding='utf-8') as archivo:
     datos_raw = json.load(archivo)
     for i in datos_raw:
         json_dump.append(i)
 
-
+# Inicializo lista con todas las asistencas
 asistencias = []
 
+# Dumpeo todas las asistencias a "asistencias"
 for i in json_dump:
     if i['asistencia_final'] == 'asistencia_final':
         continue
     asistencias.append((int(i['asistencia_final'])))
 
+# Resultados
 print(f"Asistencias : {asistencias}")
 prom_asistencias = round(sum(asistencias) / len(asistencias),2)
 print(f"La asistencia promedio del curso es {prom_asistencias}")
